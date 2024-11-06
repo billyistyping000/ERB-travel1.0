8f5026cc087a29ecf6934dc5fca8fa6261223274
billyistyping000 <bcengac@gmail.com>
2024-11-06 15:58:23 +0800

    6th_Nov
    refilled coffee&about

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 //Get the button
 const bttButton = document.getElementById("backtotop-button");
 
-// Add click event listener to the button
-bttButton.addEventListener("click", console.log ('bttButton function!!!'));
-
 // When the user clicks on the button, scroll to the top of the document
 bttButton.addEventListener("click", backToTop);
-  
-function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-}
 
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
